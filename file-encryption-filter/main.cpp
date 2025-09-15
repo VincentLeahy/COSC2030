c36d9a5c521bddf14846e54d43935b0a7a4dcebd
VincentLeahy <vincentleahy@westernwyoming.edu>
2025-09-15 01:34:37 +0000

    Ready for grading

--- file-encryption-filter/main.cpp
+++ file-encryption-filter/main.cpp
@@ -1,7 +1,57 @@
 //
-// Name
-// Date
+// Name Vincent Leahy
+// Date 09/14/2025
 // File Encryption Programming Project
 // COSC 2030
 //
 
+//File encryption Filter 
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ifstream inputFile; //input file object
+    ofstream outputFile; //output file object
+
+    string inFileName; //input file name
+    string outFileName; //output file name 
+    char data;          //To hold a character of data
+
+    //get the input file name
+    cout << "Enter the input file name: ";
+    getline(cin, inFileName);
+
+    //Get the output file name
+    cout << "Enter the output file name: ";
+    getline(cin, outFileName);
+
+    //Open the files
+    inputFile.open(inFileName.c_str());
+    outputFile.open(outFileName.c_str());
+
+    //Read the first character from the input file
+    inputFile.get(data);
+
+    //Read the contet of the input file, one character at a time, encrypt it, and store it in the output file
+    while (inputFile)
+    {
+        //Encrypt the character by adding 10 to it.
+        data += 10;
+
+        //Write the chacter to output file
+        outputFile.put(data);
+
+        //Get the next character from inputFile
+        inputFile.get(data);
+    }
+    cout << "The file has been encrypted.\n";
+
+    //close both files
+    inputFile.close();
+    outputFile.close();
+
+    return 0;
+}
