91fd25d82c8f7668aa6674cd8fe0c8cb625a2d98
VincentLeahy <vincentleahy@westernwyoming.edu>
2025-11-17 07:17:42 +0000

    Ready for grading

--- ackermanns-function/main.cpp
+++ ackermanns-function/main.cpp
@@ -1,7 +1,33 @@
 //
-// Name
-// Date
+// Name Vincent Leahy
+// Date Nob 17th 2025
 // Ackermann's Function Programming Project
 // COSC 2030
 //
 
+#include <iostream>
+using namespace std;
+
+//ackermann fucntion
+int A(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (n == 0)
+        return A (m - 1, 1);
+    else
+        return A(m - 1, A(m, n - 1));
+}
+
+int main()
+{
+    cout << "A(0, 0) = " << A(0, 0) << endl;
+    cout << "A(0, 1) = " << A(0, 1) << endl;
+    cout << "A(1, 1) = " << A(1, 1) << endl;
+    cout << "A(1, 2) = " << A(1, 2) << endl;
+    cout << "A(1, 3) = " << A(1, 3) << endl;
+    cout << "A(2, 2) = " << A(2, 2) << endl;
+    cout << "A(3, 2) = " << A(3, 2) << endl;
+
+    return 0;
+}
