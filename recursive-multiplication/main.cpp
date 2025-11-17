e43b7b29f47c2c23cc018eab123df280b4870514
VincentLeahy <vincentleahy@westernwyoming.edu>
2025-11-17 07:12:38 +0000

    Ready for grading

--- recursive-multiplication/main.cpp
+++ recursive-multiplication/main.cpp
@@ -1,7 +1,34 @@
 //
-// Name
-// Date
+// Name Vincent Leahy
+// Date Nov 17th 2025
 // Recursive Multiplication Programming Project
 // COSC 2030
 //
 
+//recursive multiplication 
+#include <iostream>
+using namespace std;
+
+//function protoype
+int multiply(int, int);
+
+int main()
+{
+    int num1, num2;
+
+    cout << "Enter 2 numbers and I will multiply them: ";
+    cin >> num1 >> num2;
+    cout << "Their Product is " << multiply(num1, num2) < end1;
+
+    return 0;
+}
+
+ int multiply(int x, int y)
+ {
+    if (x == 0 || y == 0)
+        return 0;
+    else if ( y == 1)
+        return x;
+    else
+        return x + multiply(x, y-1);
+ }
